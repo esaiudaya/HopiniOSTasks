b22c098463ff4563e5f67325a20879cb20ea12ba
Krystian Magiera <krystian.magiera@hopin.to>
2021-07-18 21:31:22 +0200

    Initial Commit

---
+++ Shared/sampletestappApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampletestappApp.swift
+//  Shared
+//
+//  Created by Krystian Magiera on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct sampletestappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
